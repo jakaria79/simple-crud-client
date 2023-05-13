a35f5d235584ec1f493fc0029ba00d4ade6de688
Jakaria <smjakaria79@gmail.com>
2023-05-13 19:37:56 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Users from './component/Users.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path:"/users",
+    element: <Users></Users>,
+    loader: () => fetch('http://localhost:5000/users')
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>,
+)
